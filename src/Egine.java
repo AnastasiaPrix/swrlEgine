1074670084fef08110008deff006561c34a98404
BoltRZA <alexbol96@yandex.ru>
2019-05-14 15:57:47 +0300

    rules Works

--- src/Egine.java
+++ src/Egine.java
@@ -5,25 +5,26 @@ import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.model.OWLOntologyStorageException;
 import org.swrlapi.core.SWRLRuleEngine;
 import org.swrlapi.factory.SWRLAPIFactory;
-
+import org.semanticweb.owlapi.model.*;
+// import org.semanticweb.owlapi.model.OWLOntologyManager.applyChanges;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.util.*;
 
 public class Egine {
 
     public static void main (String[] args) throws OWLOntologyCreationException, FileNotFoundException, OWLOntologyStorageException {
-
         OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-        File file = new File("C:\\Users\\anast\\Desktop\\magistratura\\project\\ontologies\\ont_17.owl");
+        File file = new File("C:\\Users\\User\\IdeaProjects\\rules\\src\\ont_PS.owl");
         OWLOntology ontology = manager.loadOntologyFromOntologyDocument(file);
         System.out.println("Load ontology: " + ontology);
 
         SWRLRuleEngine ruleEngine = SWRLAPIFactory.createSWRLRuleEngine(ontology);
         ruleEngine.infer();
 
-        OutputStream owl = new FileOutputStream("C:\\Users\\anast\\Desktop\\magistratura\\project\\ontologies\\ont_17_2.owl");
+        OutputStream owl = new FileOutputStream("C:\\Users\\User\\Desktop\\ont_17_2.owl");
         manager.saveOntology(ontology, owl);
 
 
